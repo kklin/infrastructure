efc406e69dc8f44289f6696d874b417bbc3fb690
Kevin Lin <kevin@quilt.io>
2017-08-07 16:08:50 -0700

    blueprint: Lint all files

--- floating-ip.js
+++ floating-ip.js
@@ -1,20 +1,20 @@
 // AWS: http://docs.aws.amazon.com/cli/latest/reference/ec2/allocate-address.html
 // Google: https://cloud.google.com/compute/docs/configure-instance-ip-addresses#reserve_new_static
-const {createDeployment, Machine, MachineRule} = require("@quilt/quilt");
-var nginx = require("@quilt/nginx");
+const { createDeployment, Machine, MachineRule } = require('@quilt/quilt');
+const nginx = require('@quilt/nginx');
 
-var floatingIp = "xxx.xxx.xxx.xxx (CHANGE ME)"
-var deployment = createDeployment({});
+const floatingIp = 'xxx.xxx.xxx.xxx (CHANGE ME)';
+const deployment = createDeployment({});
 
-var app = nginx.createService(80);
+const app = nginx.createService(80);
 
-app.place(new MachineRule(false, {floatingIp: floatingIp}));
+app.place(new MachineRule(false, { floatingIp }));
 deployment.deploy(app);
 
-var baseMachine = new Machine({
-  provider: "Amazon",
-  size: "m4.large",
-  region: "us-west-2",
+const baseMachine = new Machine({
+  provider: 'Amazon',
+  size: 'm4.large',
+  region: 'us-west-2',
   // sshKeys: githubKeys("GITHUB_USERNAME")
 });
 
